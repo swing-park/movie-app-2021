2ca83b68a8d4f409a6d5fb9d5b8a7bf8b102a5c2
swing-park <ongsim123@gmail.com>
2021-04-05 15:30:04 +0900

    Add : React-Movie-App

---
+++ src/components/Movie/Movie.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import "./Movie.css"
+
+function Movie({ id, year, title, summary, poster, genres }) {
+	return (
+		<Link to={{
+			pathname: `/movie/${id}`,
+			state: { year, title, summary, poster, genres }
+		}}>
+			<div className="movie">
+				<img src={poster} alt={title} title={title} />
+				<div className="movie-data">
+					<h3 className="movie-title">{title}</h3>
+					<h5 className="movie-year">{year}</h5>
+					<ul className="movie-genres">{genres.map((genre, idx) => (
+						<li key={idx} className="genre">{genre}</li>
+					))}
+					</ul>
+					<p className="movie-summary">{summary.slice(0, 180)}...</p>
+				</div>
+			</div>
+		</Link>
+	)
+}
+
+Movie.propTypes = {
+	id: PropTypes.number.isRequired,
+	year: PropTypes.number.isRequired,
+	title: PropTypes.string.isRequired,
+	summary: PropTypes.string.isRequired,
+	poster: PropTypes.string.isRequired,
+	genres: PropTypes.arrayOf(PropTypes.string).isRequired
+}
+
+export default Movie
---
+++ src/routes/Home.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import axios from "axios"
+import Movie from '../components/Movie/Movie.js'
+import "./Home.css"
+
+class Home extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	state = {
+		isLoading: true,
+		movies: []
+	};
+
+	async getMovies() {
+		const { data: { data: { movies } } } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+		this.setState({ movies, isLoading: false })
+	}
+
+	componentDidMount() {
+		this.getMovies();
+	}
+
+	componentDidUpdate() {
+	}
+
+	render() {
+		const { isLoading, movies } = this.state;
+
+		return (
+			<section className="container">
+				{isLoading
+					? (<div>
+						<span className="loader-text">Loading...</span>
+					</div>) :
+					(
+						<div className="movies">
+							{movies.map(movie => (
+								<Movie
+									key={movie.id}
+									id={movie.id}
+									year={movie.year}
+									title={movie.title}
+									summary={movie.summary}
+									poster={movie.medium_cover_image}
+									genres={movie.genres}
+								/>
+							))}
+						</div>
+					)
+				}
+			</section>
+		)
+	}
+}
+
+export default Home;
